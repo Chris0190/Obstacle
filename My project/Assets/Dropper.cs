27e5a71c5bc0f5cc8fe345ee270883d8626cf7fa
unknown <RHS@RHS-M2A-12.rusd.local>
2022-10-25 16:31:25 -0700

    day 5
    day 5

---
+++ My project/Assets/Dropper.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dropper : MonoBehaviour
+{
+    MeshRenderer meshren;
+    Rigiedbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshren= GetComponent<MeshRenderer>().enabled = false;
+        rb = GetComponent<Rigidbody>().useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Time.time > 5){
+        meshren.enabled = private void OnFailedToConnectToMasterServer(NetworkConnectionError error) {
+
+        }
+       } 
+    }
+}
